0c8167901d90151a4da2db3918f9a09ac28a3298
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2024-12-25 15:15:54 +0200

    Q1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Demo
+﻿using System.ComponentModel;
+
+namespace Demo
 {
     internal class Program
     {
+        static int SumArrayPassingByValue( int[] arr)
+        {
+            int sum = 0;
+            arr = new int[] { 3, 4, 5 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        static int SumArrayPassingPassingByRef(ref int[] arr) {
+            int sum = 0;
+            arr = new int[] { 3, 4, 5 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1) Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+            /* Passing by Value:
+                When passing a reference type by value the address of the object is copied to the parameter.
+                Inside the function the parameter points to the same object in memory as the original reference. 
+                Modifications to the object's contents will affect the original object. However if the parameter is assigned a new object, 
+                this change does not affect the original reference outside the function because the function has its own variable
+
+               Passing by Reference:
+                When passing a reference type by reference using the ref,
+                the actual reference (memory location) is passed. This means that changes to the parameter itself, 
+                such as assigning a new object, will affect the original reference outside the function.*/
+            int[]arr = { 1, 2, 3 };
+            Console.WriteLine(arr[0]); //1
+            SumArrayPassingByValue(arr);
+            Console.WriteLine(arr[0]); //1    called function has its own reference that point to the new array object
+            SumArrayPassingPassingByRef(ref arr);
+            Console.WriteLine(arr[0]); //     called function and main function has one memory variable location that point to the array object in heap
+
+            #endregion
         }
     }
 }
